a665a1a5140dcf6b1d417d7ab33f5dccf5c77209
Moukarnold <moukarnold@gmail.com>
2023-10-12 01:17:52 +0200

    setting for vercel

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express  from "express";
 import bodyParser from "body-parser";
 
-const app= express();
-const port = 3000;
+const app = express();
+const port = process.env.PORT || 3000;
+
 
 const toDoList = [];
 
